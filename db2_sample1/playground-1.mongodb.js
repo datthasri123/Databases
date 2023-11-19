ffaa1bfd9b7ce6d4efa3ed809e5bdb277326db04
Siva Jatla <jsmallikarjun@gmail.com>
2023-11-19 09:20:15 +0530

    commit

---
+++ db2_sample1/playground-1.mongodb.js
@@ -0,0 +1,21 @@
+
+// use('mongodbVSCodePlaygroundDB');
+
+// Insert a few documents into the sales collection.
+
+// db.getCollection('siva').insertMany([
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'},
+//     {'Name':'Siva2', 'Age':25, 'Gender':'M'}
+//     ]);
+
+
+
+    db.getCollection('siva').find()
+
+
